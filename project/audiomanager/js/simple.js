a81a3e9d553165ae0f94794e06f429944741bea7
Ben <ben@coterie.co.ke>
2016-03-09 14:19:20 +0300

    customisation and audiomanager setup

---
+++ project/audiomanager/js/simple.js
@@ -0,0 +1,12 @@
+/**
+ * @package   AudioManager
+ * @author    Ben <ben@coterie.co.ke>
+ * @link      http://coterie.co.ke
+ */
+var audiomanager = new AudioManager;
+
+audiomanager.setSCKey('29e520b21e2ce8431d8a2b0f88e84a8b');
+
+var jazzyfrenchy = audiomanager.createSound('/project/audiomanager/mp3/bensound-jazzyfrenchy.mp3','Jazzy Frenchy');
+var ofeliasdream = audiomanager.createSound('/project/audiomanager/mp3/bensound-ofeliasdream.mp3','Ofelia\'s Dream');
+var dead_combo = audiomanager.createSound('/project/audiomanager/mp3/Dead_Combo_-_01_-_Povo_Que_Cas_Descalo.mp3','Dead Combo - Povo Que Cas Descalo');
